da80700556e397b9ee51f98f7687e9c56eccf6c0
logickee <logickee@gmail.com>
2019-06-07 12:32:45 +0800

    update description,

--- main.js
+++ main.js
@@ -1,42 +1,6 @@
 searchItem = function(word){
   var query = word.selectionText;
 
-  // https://www.youtube.com/results?search_query=stm32f103
-  var searchParam = '&sp=CAISAhAB'
-  var octopartTargetUrl = "https://www.youtube.com/results?search_query=" + query + searchParam;
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
-  // https://www.adafruit.com/?q=MCP4725
-  var octopartTargetUrl = "https://www.adafruit.com?q=" + query;
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
-  // https://www.sparkfun.com/search/results?term=MCP4725
-  var octopartTargetUrl = "https://www.sparkfun.com/search/results?term=" + query;
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
-  // search in tindie
-  var octopartTargetUrl = "https://www.tindie.com/search/?q=" + query;
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
-  // https://shop.pimoroni.com/
-  var octopartTargetUrl = "https://shop.pimoroni.com/?q=" + query;
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
-  // https://www.electroschematics.com/?s=stm32f103
-  var octopartTargetUrl = "https://www.electroschematics.com/?s=" + query;
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
-  // https://hackaday.io/search?term=stm32f103
-  var octopartTargetUrl = "https://hackaday.io/search?term=" + query;
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
   // https://github.com/search?q=stm32f103
   var searchParam = '&o=desc&s=stars&type=Repositories'
   var octopartTargetUrl = "https://github.com/search?q=" + query + searchParam;
@@ -45,11 +9,6 @@ searchItem = function(word){
 
   // https://www.google.com.hk/search?q=esp-12+application+note
   var octopartTargetUrl = "https://www.google.com.hk/search?q=" + query+'+application+note';
-  console.log(octopartTargetUrl);
-  chrome.tabs.create({url: octopartTargetUrl});
-
-  // https://www.digikey.com/en/maker/search-results?k=stm32
-  var octopartTargetUrl = "https://www.digikey.com/en/maker/search-results?k=" + query;
   console.log(octopartTargetUrl);
   chrome.tabs.create({url: octopartTargetUrl});
 
@@ -70,7 +29,7 @@ searchItem = function(word){
 };
 
 chrome.contextMenus.create({
-  title: "Search parts for projects and datasheet",
+  title: "searcn parts information",
   contexts:["selection"],
   onclick: searchItem
 });
