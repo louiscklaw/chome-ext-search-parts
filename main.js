91775f5ec5132781f234350442027454a9ddd86e
logickee <logickee@gmail.com>
2019-06-07 15:27:52 +0800

    udpate for search 3d model,

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-searchItem = function(word){
+searchPartsInfo = function(word){
   var query = word.selectionText;
 
   // https://github.com/search?q=stm32f103
@@ -28,8 +28,29 @@ searchItem = function(word){
 
 };
 
+searchParts3Dmodel = function(word){
+  var query = word.selectionText;
+
+  // https://github.com/search?q=stm32f103
+  var octopartTargetUrl = "https://grabcad.com/library?utf8=%E2%9C%93&query=" + query;
+  console.log(octopartTargetUrl);
+  chrome.tabs.create({url: octopartTargetUrl});
+
+  // https://github.com/search?q=stm32f103
+  var octopartTargetUrl = "https://www.digikey.in/products/en?keywords="+query+"&pkeyword="+query+"&keywords="+query+"&v=&cad=1";
+  console.log(octopartTargetUrl);
+  chrome.tabs.create({url: octopartTargetUrl});
+
+};
+
 chrome.contextMenus.create({
   title: "searcn parts information",
   contexts:["selection"],
-  onclick: searchItem
+  onclick: searchPartsInfo
+});
+
+chrome.contextMenus.create({
+  title: "searcn parts 3d files",
+  contexts:["selection"],
+  onclick: searchParts3Dmodel
 });
